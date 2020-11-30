b975e368008814b565afea3acd4875ab293f8604
Dowland Aiello <dowlandaiello@gmail.com>
2020-11-30 17:29:55 -0500

    .config/polybar: Update polybar cfg.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,4 +2,11 @@
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-exec polybar --reload main
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
