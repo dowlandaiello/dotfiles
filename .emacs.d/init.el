a44ba5c9347bdd2c646fe28219be4f0a367f1a71
Dowland Aiello <dowlandaiello@gmail.com>
2022-07-21 22:23:36 -0400

    emacs: Add a terminal emulator.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -105,8 +105,6 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.3))
-(use-package org
-  :ensure t)
 (use-package helpful
   :ensure t
   :custom
@@ -117,6 +115,27 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
+(use-package magit
+  :ensure t)
+
+;; Shell settings
+(defun config-term ()
+  (local-set-key (kbd "M-n") 'vterm-next-prompt)
+  (local-set-key (kbd "M-p") 'vterm-previous-prompt))
+
+(use-package term
+  :ensure t
+  :config
+  (setq explicit-shell-file-name "zsh"))
+(use-package eterm-256color
+  :ensure t
+  :hook (term-mode . eterm-256color-mode))
+(use-package vterm
+  :commands vterm
+  :ensure t
+  :config
+  (setq vterm-max-scrollback 10000)
+  :hook (vterm-mode . config-term))
 
 ;; Modes
 (use-package rust-mode
@@ -130,6 +149,20 @@
 (use-package hardcore-mode
   :ensure
   :config (global-hardcore-mode))
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-switch-project-action #'projectile-dired))
+(use-package counsel-projectile
+  :ensure t
+  :config (counsel-projectile-mode))
+(use-package markdown-mode
+  :ensure t)
 
 ;; No home page
 (setq inhibit-startup-message t)
@@ -149,8 +182,57 @@
 ;; - font
 (set-fringe-mode 10)
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 80)
+(set-face-attribute 'fixed-pitch nil :font "JetBrainsMono Nerd Font" :height 80)
+(set-face-attribute 'variable-pitch nil :font "Roboto":height 90 :weight 'regular)
 (window-divider-mode +1)
 (setq window-divider-default-right-width 2 window-divider-default-bottom-width 2)
+
+;; Org Mode Config
+;; - Use sans-serif, regular fonts for non-programming sections
+(defun configure-org ()
+  (org-indent-mode)
+  (visual-line-mode 1)
+  (variable-pitch-mode 1))
+(defun stylize-org ()
+  (copy-face 'org-level-1 'ivy-org)
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Roboto" :weight 'regular :height (cdr face)))
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+(defun pad-org ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package org
+  :ensure t
+  :hook (org-mode . configure-org)
+  :config
+  (stylize-org)
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+  (setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
+  :bind (("C-c l" . org-store-link)
+         ("C-c C-l" . org-insert-link)))
+(use-package org-bullets
+  :ensure t
+  :hook (org-mode . org-bullets-mode))
+(use-package visual-fill-column
+  :ensure t
+  :hook (org-mode . pad-org))
 
 ;; Mode-specific configs
 ;; Specifically, blocking tabs in certain modes, and prefering them over spaces
@@ -193,7 +275,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(hardcore-mode helpful ivy-rich yaml-mode which-key rainbow-delimiters tree-sitter-langs tree-sitter doom-themes rust-mode all-the-icons doom-modeline counsel swiper use-package ivy)))
+   '(vterm eterm-256color org-bullets markdown-mode magit counsel-projectile projectile hardcore-mode helpful ivy-rich yaml-mode which-key rainbow-delimiters tree-sitter-langs tree-sitter doom-themes rust-mode all-the-icons doom-modeline counsel swiper use-package ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
