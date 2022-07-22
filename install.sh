d5293a2320a65cb09bb2cefc4b6c14bdfddd34f2
Dowland Aiello <dowlandaiello@gmail.com>
2022-07-21 22:26:24 -0400

    Remove unused configs.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- install.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env sh
-
-echo "== Checking for capnproto linting emacs support ==" && stat ~/emacspkg/capnproto/highlighting/emacs || (echo "\nInstalling emacs dependencies." && mkdir -p ~/emacspkg/capnproto/highlighting/emacs && curl https://raw.githubusercontent.com/capnproto/capnproto/master/highlighting/emacs/capnp-mode.el -o ~/emacspkg/capnproto/highlighting/emacs/capnp-mode.el && echo "\nDone!")
